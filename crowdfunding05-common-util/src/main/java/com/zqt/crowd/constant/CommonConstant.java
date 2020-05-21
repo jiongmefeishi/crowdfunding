866c530ceefa4ab528e322c6753287aedf791f49
zqtao <453395742@qq.com>
2020-05-21 20:52:31 +0800

    新增-MD5明文加密工具

--- crowdfunding05-common-util/src/main/java/com/zqt/crowd/constant/CommonConstant.java
+++ crowdfunding05-common-util/src/main/java/com/zqt/crowd/constant/CommonConstant.java
@@ -12,6 +12,7 @@ public class CommonConstant {
     public static final String MESSAGE_LOGIN_FAILED = "抱歉！账号密码错误！请重新输入！";
     public static final String MESSAGE_LOGIN_ACCT_ALREADY_IN_USE = "抱歉！这个账号已经被使用了！";
     public static final String MESSAGE_ACCESS_FORBIDEN = "请登录以后再访问！";
+    public static final String MESSAGE_STRING_INVALIDATE = "字符串不合法！请不要传入空字符串！";
 
     // 通用属性
     public static final String ATTR_NAME_EXCEPTION = "exception";
---
+++ crowdfunding05-common-util/src/main/java/com/zqt/crowd/util/MD5Util.java
@@ -0,0 +1,54 @@
+package com.zqt.crowd.util;
+
+import com.zqt.crowd.constant.CommonConstant;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @auther: zqtao
+ * @description: MD5 明文字符串MD5加密工具
+ * @Date: 2020/5/21 20:14
+ * @version: 1.0
+ */
+public class MD5Util {
+
+    /**
+     * 对铭文字符串进行MD5加密
+     *
+     * @param source 传入的明文字符串
+     * @return 加密结果密文
+     */
+    public static String md5(String source) {
+
+        // 1、判断有效性
+        if (source == null || source.length() == 0)
+            // 2、无效，抛异常
+            throw new RuntimeException(CommonConstant.MESSAGE_STRING_INVALIDATE);
+        try {
+            // 3、获取MessageDigest对象
+            String algorithm = "md5";
+            MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+
+            // 4、获取明文字符串字节数组
+            byte[] input = source.getBytes();
+
+            // 5、加密
+            byte[] output = messageDigest.digest(input);
+
+            // 6、创建BigInteger对象
+            int signum = 1;
+            BigInteger bigInteger = new BigInteger(signum, output);
+
+            // 7、按照16进制将bigInteger的值转换为字符串
+            int radix = 16;
+            String encoded = bigInteger.toString(radix).toUpperCase();
+            return encoded;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
