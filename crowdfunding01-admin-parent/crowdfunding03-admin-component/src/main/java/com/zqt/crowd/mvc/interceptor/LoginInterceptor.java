3fa84437d5100373d698855f8051b2f6e70233a8
zqtao <453395742@qq.com>
2020-05-23 14:29:52 +0800

    新增-登录拦截器

---
+++ crowdfunding01-admin-parent/crowdfunding03-admin-component/src/main/java/com/zqt/crowd/mvc/interceptor/LoginInterceptor.java
@@ -0,0 +1,39 @@
+package com.zqt.crowd.mvc.interceptor;
+
+import com.zqt.crowd.constant.CommonConstant;
+import com.zqt.crowd.entity.Admin;
+import com.zqt.crowd.exception.AccessForbiddenException;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @auther: zqtao
+ * @description: 登录验证拦截器，校验用户是否登录
+ * 需要去spring-web-mvc.xml 配置中配置注册拦截器
+ * @Date: 2020/5/23 13:01
+ * @version: 1.0
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        // 1.通过request对象获取Session对象
+        HttpSession session = request.getSession();
+
+        // 2.尝试从Session域中获取Admin对象
+        Admin admin = (Admin) session.getAttribute(CommonConstant.ATTR_NAME_LOGIN_ADMIN);
+
+        // 3.判断admin对象是否为空
+        if(admin == null) {
+            // 4.抛出异常
+            throw new AccessForbiddenException(CommonConstant.MESSAGE_ACCESS_FORBIDEN);
+        }
+
+        // 5.如果Admin对象不为null，则返回true放行
+        return true;
+    }
+}
---
+++ crowdfunding05-common-util/src/main/java/com/zqt/crowd/exception/AccessForbiddenException.java
@@ -0,0 +1,28 @@
+package com.zqt.crowd.exception;
+
+/**
+ * @auther: zqtao
+ * @description: 表示用户没有登录就访问受保护资源时抛出的异常
+ * @Date: 2020/5/23 12:48
+ * @version: 1.0
+ */
+public class AccessForbiddenException extends RuntimeException{
+    public AccessForbiddenException() {
+    }
+
+    public AccessForbiddenException(String message) {
+        super(message);
+    }
+
+    public AccessForbiddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccessForbiddenException(Throwable cause) {
+        super(cause);
+    }
+
+    public AccessForbiddenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
