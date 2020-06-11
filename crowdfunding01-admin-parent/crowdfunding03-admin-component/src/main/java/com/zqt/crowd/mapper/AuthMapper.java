bff41a7a2d3093fb6f39839f84150828c6a2e8e2
zqtao <453395742@qq.com>
2020-06-11 10:09:29 +0800

    逆向工程-Auth权限

---
+++ crowdfunding01-admin-parent/crowdfunding03-admin-component/src/main/java/com/zqt/crowd/mapper/AuthMapper.java
@@ -0,0 +1,31 @@
+package com.zqt.crowd.mapper;
+
+import com.zqt.crowd.entity.Auth;
+import com.zqt.crowd.entity.AuthExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AuthMapper {
+    int countByExample(AuthExample example);
+
+    int deleteByExample(AuthExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Auth record);
+
+    int insertSelective(Auth record);
+
+    List<Auth> selectByExample(AuthExample example);
+
+    Auth selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Auth record, @Param("example") AuthExample example);
+
+    int updateByExample(@Param("record") Auth record, @Param("example") AuthExample example);
+
+    int updateByPrimaryKeySelective(Auth record);
+
+    int updateByPrimaryKey(Auth record);
+}
