d7f3b0d428f27521b759308e572ed146f60d5b3c
zqtao <453395742@qq.com>
2020-06-22 09:21:04 +0800

    引入权限控制之开放登录页面和静态资源

--- crowdfunding01-admin-parent/crowdfunding03-admin-component/src/main/java/com/zqt/crowd/mvc/config/WebAppSecurityConfig.java
+++ crowdfunding01-admin-parent/crowdfunding03-admin-component/src/main/java/com/zqt/crowd/mvc/config/WebAppSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.zqt.crowd.mvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @auther: zqtao
@@ -14,6 +17,41 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 // 标记启用 web 环境下权限控制功能
 @EnableWebSecurity
-public class WebAppSecurityConfig {
+public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 请求授权
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+
+        security
+                // 1.开放登录页面
+                .authorizeRequests() // 对请求进行授权
+                .antMatchers("/admin/to/login/page.html") // 开放登录页面
+                .permitAll()                                // 无条件访问，不做任何拦截
+                // 2.开放静态资源
+                .antMatchers("/bootstrap/**")    // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/css/**")          // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/fonts/**")        // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/img/**")          // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/jquery/**")       // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/layer/**")        // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/script/**")       // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/ztree/**")        // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .antMatchers("/zqtScript/**")    // 针对静态资源进行设置，无条件访问
+                .permitAll()
+                .anyRequest()					            // 其他任意请求
+                .authenticated();                   		// 认证后访问
+
+
+    }
+
 
 }
