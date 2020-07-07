76619ea97f50ced7c951936f14f0539002229718
zqtao <453395742@qq.com>
2020-07-07 11:38:48 +0800

    通用工程新增Employee实体，引入lombok

---
+++ spring-cloud01-parent/spring-cloud02-common/src/main/java/com/zqtao/cloud/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.zqtao.cloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: zqtao
+ * @description: 测试环境：Employee 实体
+ * @Date: 2020/7/7
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    /**
+     * 雇员 id
+     */
+    private Integer empId;
+    /**
+     * 雇员名称
+     */
+    private String empName;
+    /**
+     * 雇员薪资
+     */
+    private Double empSalary;
+}
+
