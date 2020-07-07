922ed581d718043c60d76c2764e40e50fdf3af26
zqtao <453395742@qq.com>
2020-07-07 12:48:49 +0800

    测试环境：创建消费者接口

---
+++ spring-cloud01-parent/spring-cloud03-provider/src/main/java/com/zqtao/cloud/provider/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.zqtao.cloud.provider.controller;
+
+import com.zqtao.cloud.common.entity.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: zqtao
+ * @description: 测试环境：生产者控制层
+ * @Date: 2020/7/7
+ */
+@RestController
+public class EmployeeController {
+
+    @RequestMapping("get/employee")
+    public Employee getEmployee() {
+        return new Employee(1, "张三", 222.2);
+    }
+}
