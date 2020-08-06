435797cec2174669350e9ff7da2c59d477206f00
zqtao <zqtao23@163.com>
2020-08-06 18:06:40 +0800

    新增监控 dashboard工程 ，提供监控面板

---
+++ spring-cloud01-parent/spring-cloud07-dashboard/src/main/java/com/zqtao/cloud/dashboard/DashboardApplication.java
@@ -0,0 +1,19 @@
+package com.zqtao.cloud.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author: zatao
+ * 注解 @EnableHystrixDashboard 开启仪表盘监控功能
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+
+}
