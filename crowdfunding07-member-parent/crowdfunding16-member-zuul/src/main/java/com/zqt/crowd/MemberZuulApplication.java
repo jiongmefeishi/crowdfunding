cfcccb37c5e04f58b987bd87fb0a9cdcba0cc419
zqtao <zqtao23@163.com>
2020-08-13 18:05:00 +0800

    配置网关

--- crowdfunding07-member-parent/crowdfunding16-member-zuul/src/main/java/com/zqt/crowd/MemberZuulApplication.java
+++ crowdfunding07-member-parent/crowdfunding16-member-zuul/src/main/java/com/zqt/crowd/MemberZuulApplication.java
@@ -2,11 +2,14 @@ package com.zqt.crowd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author: zqtao
  * @description: 网关微服务
+ * 注解 @EnableZuulProxy 开启网关服务
  */
+@EnableZuulProxy
 @SpringBootApplication
 public class MemberZuulApplication {
 
