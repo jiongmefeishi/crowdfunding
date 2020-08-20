02b789722f797344af1d30c08a45d6b5b27dc32f
zqtao <zqtao23@163.com>
2020-08-20 18:01:20 +0800

    视图配置

---
+++ crowdfunding07-member-parent/crowdfunding12-member-authentication-consumer/src/main/java/com/zqt/crowd/config/MyWebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.zqt.crowd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: zqtao
+ * @description: MVC配置类
+ */
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 浏览器访问的地址
+        String urlPath = "/auth/member/to/reg/page";
+
+        // 目标视图的名称，将来拼接“prefix: classpath:/templates/”、“suffix: .html”前后缀
+        String viewName = "member-reg";
+
+        // 添加view-controller
+        registry.addViewController(urlPath).setViewName(viewName);;
+        registry.addViewController("/auth/member/to/login/page").setViewName("member-login");
+        registry.addViewController("/auth/member/to/center/page").setViewName("member-center");
+    }
+}
