46e42edf9b576316276ab990281a7d449a16853e
zqtao <zqtao23@163.com>
2020-08-21 16:38:59 +0800

    测试redis

--- crowdfunding07-member-parent/crowdfunding11-member-redis-provider/src/test/java/com/zqt/crowd/RedisTest.java
+++ crowdfunding07-member-parent/crowdfunding11-member-redis-provider/src/test/java/com/zqt/crowd/RedisTest.java
@@ -1,6 +1,8 @@
 package com.zqt.crowd;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -12,10 +14,13 @@ class RedisTest {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
+    private Logger logger = LoggerFactory.getLogger(RedisTest.class);
+
     @Test
     void testRedis(){
         ValueOperations<String, String> operations = redisTemplate.opsForValue();
-        operations.set("apple", "xiaoxiao");
+//        operations.set("apple11", "xiaoxiao1");
+        logger.info(operations.get("SMS_REDIS_CODE_PREFIX_15670255898"));
     }
 
 }
