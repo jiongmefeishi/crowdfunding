9cde3367b5ab8273b3f4d414d4b40b850914163c
zqtao <zqtao23@163.com>
2020-09-16 13:53:23 +0800

    测试项目详情查询

--- crowdfunding07-member-parent/crowdfunding10-member-mysql-provider/src/test/java/com/zqt/crowd/mapper/project/ProjectPOMapperTest.java
+++ crowdfunding07-member-parent/crowdfunding10-member-mysql-provider/src/test/java/com/zqt/crowd/mapper/project/ProjectPOMapperTest.java
@@ -1,6 +1,7 @@
 package com.zqt.crowd.mapper.project;
 
 import com.zqt.crowd.entity.vo.portal.DetailProjectVO;
+import com.zqt.crowd.entity.vo.portal.DetailReturnVO;
 import com.zqt.crowd.entity.vo.portal.PortalProjectVO;
 import com.zqt.crowd.entity.vo.portal.PortalTypeVO;
 import lombok.extern.slf4j.Slf4j;
@@ -22,8 +23,38 @@ class ProjectPOMapperTest {
     @Test
     void testSelectDetailProjectVO() {
         String projectId = "17";
+
         DetailProjectVO detailProjectVO = projectPOMapper.selectDetailProjectVO(projectId);
-        log.info(detailProjectVO.toString());
+
+        log.info(detailProjectVO.getProjectId() + "");
+        log.info(detailProjectVO.getProjectName());
+        log.info(detailProjectVO.getProjectDescription());
+        log.info(detailProjectVO.getFollowerCount() + "");
+        log.info(detailProjectVO.getStatus() + "");
+        log.info(detailProjectVO.getMoney() + "");
+        log.info(detailProjectVO.getSupportMoney() + "");
+        log.info(detailProjectVO.getPercentage() + "");
+        log.info(detailProjectVO.getDeployDate() + "");
+        log.info(detailProjectVO.getSupporterCount() + "");
+        log.info(detailProjectVO.getHeaderPicturePath());
+
+        List<String> detailPicturePathList = detailProjectVO.getDetailPicturePathList();
+        for (String path : detailPicturePathList) {
+            log.info("detail path=" + path);
+        }
+
+        List<DetailReturnVO> detailReturnVOList = detailProjectVO.getDetailReturnVOList();
+        for (DetailReturnVO detailReturnVO : detailReturnVOList) {
+            log.info(detailReturnVO.getReturnId() + "");
+            log.info(detailReturnVO.getSupportMoney() + "");
+            log.info(detailReturnVO.getSignalPurchase() + "");
+            log.info(detailReturnVO.getPurchase() + "");
+            log.info(detailReturnVO.getSupporterCount() + "");
+            log.info(detailReturnVO.getFreight() + "");
+            log.info(detailReturnVO.getReturnDate() + "");
+            log.info(detailReturnVO.getContent() + "");
+            log.info(detailReturnVO.getFreight() + "");
+        }
     }
 
 
