6c08388f552e40b408813e5bc37e3fe5b669bffc
zqtao <453395742@qq.com>
2020-10-06 22:59:18 +0800

    支付

--- crowdfunding07-member-parent/crowdfunding15-member-pay-consumer/src/main/java/com/zqt/crowd/MemberPayConsumerApplication.java
+++ crowdfunding07-member-parent/crowdfunding15-member-pay-consumer/src/main/java/com/zqt/crowd/MemberPayConsumerApplication.java
@@ -1,17 +1,23 @@
 package com.zqt.crowd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author: zqtao
  * @description: 支付功能微服务
  */
+@Slf4j
+@EnableFeignClients
 @SpringBootApplication
 public class MemberPayConsumerApplication {
 
     public static void main(String[] args) {
+        log.info("{} 开始加载 ===>>>", "支付功能微服务");
         SpringApplication.run(MemberPayConsumerApplication.class, args);
+        log.info("{} 加载完毕 ===>>>", "支付功能微服务");
     }
 
 }
